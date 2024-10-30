bfb007f59a726ddab29b859cc8f28f584b984bd7
Marvin <marvinlaixs@gmail.com>
2024-10-29 22:23:01 -0600

    v1 funcional

--- pregunta.js
+++ pregunta.js
@@ -1,6 +1,6 @@
 function corazon() {
-  // Definir el patrón de corazón con asteriscos en un array de strings
   const heartPattern = [
+    "Te amo",
     "  ***     ***  ",
     "*****   *****",
     "******* *******",
@@ -13,6 +13,28 @@ function corazon() {
     "      *       ",
   ];
 
-  // Combinar el array en una cadena con saltos de línea y mostrarlo
   document.getElementById("output").textContent = heartPattern.join("\n");
+}
+
+let counter = 0;
+
+function toggleContent() {
+  const display = document.getElementById("output");
+  const youtubePlayer = document.getElementById("youtubePlayer");
+
+  if (counter === 0) {
+    display.textContent =
+      "Ni todas las estrellas juntas brillan tanto como tú preciosaa!🌟";
+  } else if (counter === 1) {
+    display.textContent = "Aparte de linda, hermosaaaaaaaaa!❤️";
+  } else if (counter === 2) {
+    display.textContent =
+      "Eres como el sol al amanacerrrrr... iluminas todo a tu alrededor✨";
+  } else if (counter === 3) {
+    display.textContent = "PD: Te quiero muuuuuuuuuu-chooooooo✨";
+  } else if (counter === 4) {
+    youtubePlayer.style.display = "block";
+  }
+
+  counter = (counter + 1) % 5;
 }
